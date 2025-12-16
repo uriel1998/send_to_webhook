f8674117429b456878404738ded44ab4e4b830d0
Steven Saus <steven@stevesaus.com>
2025-12-16 16:27:17 -0500

    chore: add .gitignore file to exclude unnecessary files from version control feat: create maubot_vars.env.example for environment variable configuration feat: add send_to_webhook.sh script for posting notifications to maubot

---
+++ send_to_webhook.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+export INSTALL_DIR="$(dirname "$(readlink -f "$0")")"
+source "${SCRIPT_DIR}/maubot_vars.env"
+LOUD=1
+
+function loud() {
+    if [ "${LOUD:-0}" -eq 1 ];then
+        echo "$@"
+    fi
+}
+
+
+if [ $# -eq 0 ]; then
+    # no arguments passed, use stdin
+    jbody=$(cat)
+else
+	while [ $# -gt 0 ]; do
+		option="$1"
+		case $option in
+		--loud)
+			LOUD=1
+			shift
+			;;
+		--title)
+			shift
+			title="${1}"
+			shift
+			;;
+		--body)
+			shift
+			body="${@}"
+			shift
+			;;
+		*) shift ;;
+		esac
+	done
+	if [ -z "${jtitle}" ];then
+		jtitle="Notification!"
+	fi
+	if [ -z "${jbody}" ];then
+		jbody=" "
+	fi
+	loud "[info] Posting reply to maubot"
+#   Build the JSON safely with jq
+	json_payload=$(jq -n --arg title "${jtitle}" --arg body "${jbody}" '{ title: $title,body: $body}')
+
+	# Then send it with curl
+	curl -X POST -H "Content-Type: application/json" -u abc:123 "${MATRIXSERVER}/_matrix/maubot/plugin/${MAUBOT_STATUS_WEBHOOK_INSTANCE}/send" -d "$json_payload"
+	loud "[info] Posted to maubot"  
+
+fi
